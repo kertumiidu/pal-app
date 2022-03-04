aba0773eca59c16727a03c1b13cbd8d325efcb67
Kertu Miidu <kertu@progmatic.ee>
2022-03-04 22:49:23 +0200

    test

--- src/main/java/ee/pall/app/AppApplication.java
+++ src/main/java/ee/pall/app/AppApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AppApplication {
 
 	public static void main(String[] args) {
+
+		System.out.println("Hello, world!");
 		SpringApplication.run(AppApplication.class, args);
 	}
 
